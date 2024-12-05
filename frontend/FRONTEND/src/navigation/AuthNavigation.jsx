83f547b2f94d39c393530cdb455374a829d47968
julian343434 <julher2000@hotmail.es>
2024-12-05 14:18:32 -0500

    se creo la vista de perfil tanto de comprador como el vendedor

--- frontend/FRONTEND/src/navigation/AuthNavigation.jsx
+++ frontend/FRONTEND/src/navigation/AuthNavigation.jsx
@@ -2,13 +2,14 @@ import Login from "../pages/authentication/Login";
 import Register from "../pages/authentication/RegisterPage";
 import DeniedPage from "../pages/other/Denied";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Externo from "../pages/externo/Externo"
+//import Externo from "../pages/externo/Externo"
+import Test from "../pages/test/Test"
 
 export default function AuthNavigation() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Externo/>} />
+                <Route path="/" element={<Test/>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />}/>
                 <Route path="*" element={<DeniedPage />} />
