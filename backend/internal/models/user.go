7887f17402ca33d85c05f80b447968806df2c17d
Unikyri <juan.arango.quijano@unillanos.edu.co>
2024-12-05 14:41:22 -0500

    Ya se crearon los modelos de datos

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,1 +1,15 @@
 package models
+
+type Usuario struct {
+	ID          uint   `gorm:"primaryKey"`
+	Correo      string `gorm:"unique;not null"`
+	Contraseña  string `gorm:"not null"`
+	Coordenadas string `gorm:"not null"`
+	Telefono    string
+	Nombre      string `gorm:"not null"`
+	Rol         string `gorm:"not null"`
+	Foto        string
+	Descripcion string
+	Productos   []Producto     `gorm:"foreignKey:FKUsuario"`
+	Reseñas     []Calificacion `gorm:"foreignKey:FKUsuario"`
+}
