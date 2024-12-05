1b00db86e836a8d262c559a696c1d9005d72c997
julian343434 <julher2000@hotmail.es>
2024-12-05 16:31:27 -0500

    se configuro el layout de los usuarios

--- frontend/FRONTEND/src/pages/externo/Externo.jsx
+++ frontend/FRONTEND/src/pages/externo/Externo.jsx
@@ -1,12 +1,11 @@
-import InteractiveCurriculum from '../../components/InteractiveCurriculum';
+
 import Layout from '../../layouts/ExternalLayout';
 
 const Externo = () => {
   return (
     <Layout>
       <div className="w-full  text-white  text-center">
-        
-        <InteractiveCurriculum/>
+
       </div>
       
     </Layout>
--- frontend/FRONTEND/src/pages/home/Home.jsx
+++ frontend/FRONTEND/src/pages/home/Home.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import SideNav from '../../components/SideNav';
-import { Outlet } from 'react-router-dom';
+
 const Home = () => {
 	return (
-		<div className="flex 100vh 100wh h-full w-full">
-			<SideNav />
-			<div className="flex justify-center w-full h-full items-center"></div>
-		</div>
+<></>
+		
 	);
 };
 
