ce0f87de8cc0dd00f9610cede503acfa4bdcf0e7
Unikyri <juan.arango.quijano@unillanos.edu.co>
2024-12-05 13:54:00 -0500

    Se está cambiando la conexión a la BD usando GORM

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,28 +1,40 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var DB *sql.DB
+var DB *gorm.DB
 
+// InitDB configura la conexión a la base de datos usando GORM
 func InitDB() {
+	// Cadena de conexión
 	connStr := fmt.Sprintf(
 		"host=192.168.140.128 port=5432 user=hackathon password=hack15243 dbname=reto2 sslmode=disable",
 	)
 
+	// Intentar abrir la conexión usando GORM
 	var err error
-	DB, err = sql.Open("postgres", connStr)
+	DB, err = gorm.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Error al conectar a la base de datos: %v\n", err)
 	}
 
-	if err = DB.Ping(); err != nil {
+	// Verificar la conexión
+	if err = DB.DB().Ping(); err != nil {
 		log.Fatalf("Error al hacer ping a la base de datos: %v\n", err)
 	}
 
 	log.Println("Conexión a la base de datos exitosa")
+}
+
+// CerrarDB cierra la conexión a la base de datos cuando ya no se necesite
+func CerrarDB() {
+	if err := DB.Close(); err != nil {
+		log.Fatalf("Error al cerrar la base de datos: %v\n", err)
+	}
 }
