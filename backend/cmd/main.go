47937233186a057279e60f8f66ecb01bf1b0ad20
Unikyri <juan.arango.quijano@unillanos.edu.co>
2024-12-06 00:29:30 -0500

    Conectando el servidor backend con la ip externa del VPS

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"Hackathon/db"
+	"github.com/gofiber/fiber/v2" // Asumiendo que estás usando Fiber v2
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"os/exec"
-
-	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -20,6 +20,20 @@ func main() {
 	}
 
 	log.Println("Servidor listo para iniciar...")
+
+	// Inicializa el servidor Fiber
+	app := fiber.New()
+
+	// Configura las rutas
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("¡Servidor corriendo!")
+	})
+
+	// Inicia el servidor en la IP externa (0.0.0.0) y puerto 10000
+	err := app.Listen(":10000") // Aquí puedes cambiar el puerto si es necesario
+	if err != nil {
+		log.Fatalf("Error al iniciar el servidor: %v\n", err)
+	}
 }
 
 func runMigrations() error {
