6ad26307bae6d94780315e9f9564605d48a9ae2d
deivid <julher2000@hotmail.es>
2024-12-06 18:14:47 -0500

    se configuraron los servicios login y register

--- frontend/FRONTEND/vite.config.js
+++ frontend/FRONTEND/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  server: {
+    host: '192.168.1.11', // O usa '0.0.0.0' para accesos desde cualquier red
+    port: 5173, // Mantén el puerto para tu frontend
+    
+  },
 });
