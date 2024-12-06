dd2b9838ce1b1deb66c21274ecfb502742ad890e
JhonatanFerrer <jhferrer11@hotmail.com>
2024-12-06 09:25:23 -0500

    profile visualizer handler creado

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -7,8 +7,12 @@ import (
 
 // SetupRoutes configura las rutas de la API
 func SetupRoutes(app *fiber.App) {
-	app.Post("/register", handlers.Register)                // Ruta para registrar usuarios
-	app.Post("/login", handlers.Login)                      // Ruta para iniciar sesión
-	app.Get("/userVisualizer", handlers.VisualizarUsuarios) // Ruta para iniciar sesión
+	//Registro y Login
+	app.Post("/register", handlers.Register)
+	app.Post("/login", handlers.Login)
+
+	//Visualizacion de interfaz
+	app.Get("/userVisualizer", handlers.VisualizarUsuarios)
+	app.Get("/profileVisualizer", handlers.ObtenerInfoUsuario)
 
 }
