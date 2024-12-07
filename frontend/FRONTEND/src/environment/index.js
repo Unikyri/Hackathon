5c6771c8ca3c31f36f29353c4444acde9674601d
deivid <julher2000@hotmail.es>
2024-12-07 14:45:48 -0500

    preparando para poder hacer deploy

--- frontend/FRONTEND/src/environment/index.js
+++ frontend/FRONTEND/src/environment/index.js
@@ -2,7 +2,7 @@
 export const BASE_URL = 'http://34.51.10.74:10000';
 export const BASE_URL_DEPLOY ='34.51.10.74'
 
-export const BASE_URL_DEV ='192.168.1.11'
+export const BASE_URL_DEV ='10.224.0.2'
 
 export const BASE_COLORS = 'black';
 
