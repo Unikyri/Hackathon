5182810ef4a493d7ba2c3807e862a33fccb8a71b
deivid <julher2000@hotmail.es>
2024-12-07 19:51:21 -0500

    se configuro la vista del perfil

--- frontend/FRONTEND/src/navigation/MainNavigation.jsx
+++ frontend/FRONTEND/src/navigation/MainNavigation.jsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
 import Home from "../pages/home/Home";
 import UserAdmin from "../pages/home/UserAdmin";
 import PageNotFound from "../pages/other/PageNotFound";
 import FavouritesSeller from "../pages/favourites/FavouritesSeller";
-import {Comprador, UserProfile} from "../pages/index";
+import { Comprador, Vendedor, UserProfile } from "../pages/index";
+import { AuthContext } from "../providers/AuthProvider";
 
-// import {Vendedor, comprador} from '../pages';
 export default function MainNavigation() {
+    const { userRole} = useContext(AuthContext);
+
+    // Función para renderizar el componente basado en el rol
+    const renderRoleBasedComponent = () => {
+        if (userRole === "comprador") {
+            return <Comprador />;
+        } else if (userRole === "vendedor") {
+            return <Vendedor />;
+        } else {
+            // Redirigir a una página no autorizada si el rol no es válido
+            return <Navigate to="/not-authorized" />;
+        }
+    };
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<Home/>}/>   
+                <Route path="/home" element={<Home />} />
                 <Route path="/favoritos" element={<FavouritesSeller />} />
-                <Route path="/perfil" element={<Comprador/>} />
+                {/* Ruta dinámica para renderizar basado en el rol */}
+                <Route path="/perfil" element={renderRoleBasedComponent()} />
                 <Route path="/profile/:name" element={<UserProfile />} />
                 <Route path="/admin" element={<UserAdmin />} />
-                {/* <Route path="/perfil" element={<Vendedor />} /> */}
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
